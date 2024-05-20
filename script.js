386f65c3f44d98c8db38470851ef0f8839cb861a
VamanaPed <168412411+VamanaPed@users.noreply.github.com>
2024-05-20 15:12:01 +1200

    Add playRound, playGame functions Added playRound function to calculate winner, and playGame which is 5 rounds, changed player input to change all letters to caps before parsing

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ console.log("Hello World");
 
 let lastChoice = "rock";
 
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice()
 {
     let choice = "";
@@ -44,26 +47,25 @@ function getHumanChoice(text)
     let input = prompt(text);
     console.log("Player entered: " + input);
 
+    input = input.toUpperCase();
+
     switch (input)
     {
-        case "Rock":
-        case "rock":
-        case "r":
+        case "ROCK":
+        case "R":
                 input = "rock";
             break;
 
-        case "Paper":
-        case "paper":
-        case "p":
+        case "PAPER":
+        case "P":
                 input = "paper";
             break;
 
-        case "Scissors":
-        case "scissors":
-        case "s":
+        case "SCISSORS":
+        case "S":
                 input = "scissors";
             break;
-            
+
         default:
             input = "";
             break;
@@ -77,9 +79,101 @@ function getHumanChoice(text)
     return input;
 }
 
-getHumanChoice();
-
-for (i = 0; i < 10; i++)
+function playRound(humanChoice, computerChoice)
 {
-    console.log(getComputerChoice());
+    let winner = "tie";
+    let message = "Tie! Try again";
+
+    console.log("Player: " + humanChoice + " Computer: " + computerChoice);
+
+    if(humanChoice === "rock")
+    {
+        if(computerChoice === "rock")
+        {
+            message = "Tie! Rock and Rock!";
+        }
+        else if(computerChoice === "paper")
+        {
+            winner = "computer";
+            message = "You Lose! Paper beats Rock!";
+        }
+        else if(computerChoice = "scissors")
+        {
+            winner = "player";
+            message = "You Win! Rock beats scissors!";
+        }
+    }
+    else if(humanChoice === "paper")
+    {
+        if(computerChoice === "rock")
+        {
+            winner = "player";
+            message = "You Win! Paper beats Rock!";
+        }
+        else if(computerChoice === "paper")
+        {
+            message = "Tie! Paper and Paper!";
+        }
+        else if(computerChoice = "scissors")
+        {
+            winner = "computer";
+            message = "You Lose! Scissors beats Paper!";
+        }
+    }
+    else if(humanChoice = "scissors")
+    {
+        if(computerChoice === "rock")
+        {
+            winner = "computer";
+            message = "You Loose! Rock beats Scissors!";
+        }
+        else if(computerChoice === "paper")
+        {
+            winner = "player";
+            message = "You Win! Scissors beats Paper!";
+        }
+        else if(computerChoice = "scissors")
+        {
+            message = "Tie! Scissors and Scissors!";
+        }
+    }
+
+    switch(winner)
+    {
+        case "player":
+                playerScore ++;
+            break;
+        case "computer":
+                computerScore ++;
+            break;
+    }
+
+    console.log("Player Score: " + playerScore + " Computer Score: " + computerScore);
+    alert(message);
 }
+
+function playGame()
+{
+    for (i = 0; i < 5; i++)
+    {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if(playerScore > computerScore)
+    {
+        console.log("Player Wins: " + playerScore + " > " + computerScore);
+        alert("You Win! Your score: " + playerScore + " Computer score: " + computerScore);
+    }
+    else if (computerScore > playerScore)
+    {
+        console.log("Computer Wins: " + computerScore + " > " + playerScore);
+        alert("You Loose! Computer score: " + computerScore + " Your score: " + playerScore);
+    }
+    else
+    {
+        console.log("Draw: Computer: " + computerScore + " Player: " + playerScore);
+        alert("Draw! Your score: " + playerScore + " Computer score: " + computerScore);
+    }
+}
+
+playGame();
